e52f4b790f1324cbb2bd527827a19713b7abefee
Ryan Resella <ryan@codeforamerica.org>
2011-07-18 15:32:10 -0700

    Add Basic Info form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,24 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     render :template => 'users/profile/show'
   end
+  
+  def edit
+    @user = User.find(params[:id])
+    render :template => 'users/profile/edit'
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    
+    respond_to do |format|
+         if @user.update_attributes(params[:user])
+           format.html { redirect_to(@user, :notice => 'User was successfully updated.') }
+           format.xml  { head :ok }
+         else
+           format.html { render :action => "edit" }
+           format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+         end
+    end
+  end
+  
 end
