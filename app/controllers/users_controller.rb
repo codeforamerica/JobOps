ce8e8ec3829ee6b706515dd7c6ed5d64cb130016
Ryan Resella <ryan@codeforamerica.org>
2011-07-18 14:08:22 -0700

    Added routing for profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    render :template => 'users/profile/show'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ JobOps::Application.routes.draw do
 
   devise_for :users
 
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
