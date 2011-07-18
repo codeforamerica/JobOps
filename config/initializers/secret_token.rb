daf21bec08497ff211d8180819442d22579b8a22
Ryan Resella <ryan@codeforamerica.org>
2011-07-18 10:55:56 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobOps::Application.config.secret_token = '9ecac99dd79f18db1ef73d22588866aabfea91a3b30e6af4d408c2c68e31d0fa9df58aea7251ebe13d35625bff044508d7244ce58f63124bb7bce5bfe673f460'
