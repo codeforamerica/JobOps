b49e0e2fc11549d949be10a2d594b5568e9d526e
Ryan Resella <ryan@codeforamerica.org>
2011-07-19 15:33:58 -0700

    Changed view to date select for dob

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110719221107) do
+ActiveRecord::Schema.define(:version => 20110719222816) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -62,10 +62,10 @@ ActiveRecord::Schema.define(:version => 20110719221107) do
     t.string   "goal"
     t.string   "relocate"
     t.string   "desired_salary"
-    t.integer  "age"
     t.string   "gender"
     t.string   "ethnicity"
     t.string   "family"
+    t.date     "dob"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
