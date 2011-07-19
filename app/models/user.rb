5bb2bf1e8b9472dca295b49360a4fd109ea8fe25
Ryan Resella <ryan@codeforamerica.org>
2011-07-19 15:35:54 -0700

    Added dob to accessible attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :city, :state, :goal, :relocate, :desired_salary, :age, :gender,:ethnicity,:family
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :city, :state, :goal, :relocate, :desired_salary, :gender,:ethnicity,:family, :dob
+
 end
