d474727f01e5739e3d414979d1b1a2937d1d7520
Ryan Resella <ryan@codeforamerica.org>
2011-07-19 14:31:21 -0700

    Wrote specs for edit and update in User controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,4 +10,23 @@ describe UsersController do
       response.should render_template("users/profile/show")
     end
   end
+  
+  describe "edit action should render show template" do
+    it "should be successful" do
+      get :edit, :id => @user.id
+      response.should render_template("users/profile/edit")
+    end
+  end
+  
+  describe '#update' do
+    before do
+      @user = Factory(:user)
+      put :update, :id => @user.id, :user => { :first_name => "Example" }
+      @response = response
+    end
+
+    it "should update a person" do
+      @user.reload.first_name.should == "Example"
+    end
+  end
 end
