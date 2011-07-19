73e16511def9c43c0620aec2fa508ee4768f4b6a
Ryan Resella <ryan@codeforamerica.org>
2011-07-19 13:36:07 -0700

    Wrote spec for show on user controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe UsersController do
-
+  
+  login_user
+  
+  describe "show action should render show template" do
+    it "should be successful" do
+      get :show, :id => @user.id
+      response.should render_template("users/profile/show")
+    end
+  end
 end
