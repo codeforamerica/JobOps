c6be37ae8ba98dc602be12774c50d7bebbc869e6
Ryan Resella <ryan@codeforamerica.org>
2011-07-19 16:17:07 -0700

    Added validation for org_name, title, start_date

--- app/models/job_history.rb
+++ app/models/job_history.rb
@@ -1,4 +1,8 @@
 class JobHistory < ActiveRecord::Base
   belongs_to :user
   has_one :user
+  
+  validates_presence_of :org_name
+  validates_presence_of :title
+  validates_presence_of :start_date
 end
