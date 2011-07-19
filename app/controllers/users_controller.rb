f41acbd29823446e02956e76582e1bc892bed888
Ryan Resella <ryan@codeforamerica.org>
2011-07-19 13:35:49 -0700

    Added authentication filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  
+  before_filter :authenticate_user!
+  
   def show
     @user = User.find(params[:id])
     render :template => 'users/profile/show'
