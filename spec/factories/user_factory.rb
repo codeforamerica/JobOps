df6da20e795cb823ac5d27de28566ce7435ba129
Ryan Resella <ryan@codeforamerica.org>
2011-07-19 14:29:20 -0700

    Added first and lastname to User factory

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,4 +2,6 @@ Factory.define(:user) do |u|
   u.sequence(:email) {|n| "email#{n}@testcompany.com"}
   u.password "123456"
   u.password_confirmation "123456"
+  u.first_name "GI"
+  u.last_name "Joe"
 end
