7386e6bb8ef36d2e0c81ab99f0d3da6d4eb84c5b
Ryan Resella <ryan@codeforamerica.org>
2011-07-20 15:23:47 -0700

    Added spec to check for valid year

--- spec/models/education_spec.rb
+++ spec/models/education_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Education do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "check if valid year" do
+    year = Education.new.valid_year(Date.today)
+    year.should == Date.today.year
+  end
+  
+  describe "blank year should return nil" do
+    year = Education.new.valid_year(nil)
+    year.should == nil
+  end
 end
