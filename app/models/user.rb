e7cd5141096d504d17e265f4210cd30180127b36
Ryan Resella <ryan@codeforamerica.org>
2011-07-20 13:36:51 -0700

    Added relationship on Users for Education

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :city, :state, :goal, :relocate, :desired_salary, :gender,:ethnicity,:family, :dob
 
   has_many :job_histories
+  has_many :educations
 end
