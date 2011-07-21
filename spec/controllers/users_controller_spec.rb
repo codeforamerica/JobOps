45add4376bd94f6fde50a1e4142704167379eeb8
Ryan Resella <ryan@codeforamerica.org>
2011-07-21 14:55:07 -0700

    Wrote specs for military info on user

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -21,12 +21,24 @@ describe UsersController do
   describe '#update' do
     before do
       @user = Factory(:user)
-      put :update, :id => @user.id, :user => { :first_name => "Example" }
-      @response = response
+      put :update, :id => @user.id, :user => { :first_name => "Example",
+        :military_status => "Active",
+        :service_branch => "Army",
+        :moc => "22b",
+        :rank => "032",
+        :disability => "None",
+        :security_clearance => "Top Secret"
+        }
     end
 
     it "should update a person" do
       @user.reload.first_name.should == "Example"
+      @user.reload.military_status.should == "Active"
+      @user.reload.service_branch.should == "Army"
+      @user.reload.moc.should == "22b"
+      @user.reload.rank.should == "032" 
+      @user.reload.disability.should == "None" 
+      @user.reload.security_clearance.should == "Top Secret"
     end
   end
 end
