329e0449d48443dc0df4527d13900a739acebd41
Ryan Resella <ryan@codeforamerica.org>
2011-07-20 17:15:11 -0700

    Added spec for check_prsent

--- spec/models/job_history_spec.rb
+++ spec/models/job_history_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe JobHistory do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "return date when date is present" do
+    present = JobHistory.new.check_present(Date.today)
+    present.should == Date.today
+  end
+  
+  describe "return present when date is nil" do
+    present = JobHistory.new.check_present(nil)
+    present.should == "Present"
+  end
+  
+  
+  
 end
