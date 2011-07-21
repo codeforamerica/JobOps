ef54dc6978caa6bd1c01c348b0d4f88d49a91cf7
Ryan Resella <ryan@codeforamerica.org>
2011-07-20 17:16:11 -0700

    Implemented check_present turns database nil dates to Present

--- app/models/job_history.rb
+++ app/models/job_history.rb
@@ -5,4 +5,12 @@ class JobHistory < ActiveRecord::Base
   validates_presence_of :org_name
   validates_presence_of :title
   validates_presence_of :start_date
+  
+  def check_present(check_date)
+    if check_date.nil?
+      value = "Present"
+    else
+      value = check_date
+    end
+  end
 end
