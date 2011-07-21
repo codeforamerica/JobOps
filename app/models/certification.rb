9328f05478802c4c0d19671151aab31e88cf6ff3
Ryan Resella <ryan@codeforamerica.org>
2011-07-20 18:31:30 -0700

    Added relationship for User and Certifications

--- app/models/certification.rb
+++ app/models/certification.rb
@@ -1,2 +1,4 @@
 class Certification < ActiveRecord::Base
+  belongs_to :user
+  has_one :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :city, :state, :goal, :relocate, :desired_salary, :gender,:ethnicity,:family, :dob
 
+  has_many :certifications
   has_many :job_histories
   has_many :educations
   has_many :skills
