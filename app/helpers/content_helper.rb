f943d2817309b42e06b79bf0427696b5f46f9122
Ryan Resella <ryan@codeforamerica.org>
2011-07-26 12:51:44 -0700

    Created helper file for devise to show forms in different locations

---
+++ app/helpers/content_helper.rb
@@ -0,0 +1,13 @@
+module ContentHelper
+  def resource_name
+    :user
+  end
+ 
+  def resource
+    @resource ||= User.find_by_id(current_user.id)
+  end
+ 
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+end
