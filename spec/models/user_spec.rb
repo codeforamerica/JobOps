dc4ea0897886e82002539068c30f51a26f8dd797
Ryan Resella <ryan@codeforamerica.org>
2011-07-25 17:34:58 -0700

    Wrote spec for users full name full_name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before do
+    @user = Factory(:user)
+  end
+  
+  describe "full_name" do
+    it "should combine the first name and last name" do
+      name = @user.full_name
+      name.should == "GI Joe"
+    end
+  end
 end
