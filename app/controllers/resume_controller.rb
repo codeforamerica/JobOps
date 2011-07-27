1f3e9c8ec86e69f51fa062f329ec93ad2f4e4e5e
Ryan Resella <ryan@codeforamerica.org>
2011-07-27 13:19:42 -0700

    Added edit to resume controller

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -11,6 +11,16 @@ class ResumeController < ApplicationController
   # PUT /resumes/1
   # PUT /resumes/1.json
   def update
-
+    @user = User.find_by_id(current_user.id)
+    
+    respond_to do |format|
+         if @user.update_attributes(params[:user])
+           format.html { redirect_to(@user, :notice => 'User was successfully updated.') }
+           format.xml  { head :ok }
+         else
+           format.html { render :action => "edit" }
+           format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+         end
+    end
   end
 end
