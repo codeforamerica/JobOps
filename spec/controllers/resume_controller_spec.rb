fa53b8242e5ea8b4bf01f6ac084c8e0052304fd8
Ryan Resella <ryan@codeforamerica.org>
2011-07-27 15:17:50 -0700

    Wrote specs for resume controller

--- spec/controllers/resume_controller_spec.rb
+++ spec/controllers/resume_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe ResumeController do
+  
+  login_user
 
   describe "GET 'index'" do
     it "should be successful" do
@@ -8,5 +10,23 @@ describe ResumeController do
       response.should be_success
     end
   end
+  
+  describe "edit action should render show template" do
+    it "should be successful" do
+      get :edit, :id => @user.id
+      response.should render_template("resume/edit")
+    end
+  end
+  
+  describe '#update' do
+    before do
+      @user = Factory(:user)
+      put :update, :id => @user.id, :user => { :resume => "I have updated my resume"
+        }
+    end
 
+    it "should update the users resume" do
+      @user.reload.first_name.should == "I have updated my resume"
+    end
+  end
 end
