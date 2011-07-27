1e135e3909fdc8af37668fc7aa0b2b960a991407
Ryan Resella <ryan@codeforamerica.org>
2011-07-27 16:27:52 -0700

    Update spec to check the correct field for resume

--- spec/controllers/resume_controller_spec.rb
+++ spec/controllers/resume_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe ResumeController do
-  
+
   login_user
 
   describe "GET 'index'" do
@@ -10,23 +10,22 @@ describe ResumeController do
       response.should be_success
     end
   end
-  
+
   describe "edit action should render show template" do
     it "should be successful" do
       get :edit, :id => @user.id
       response.should render_template("resume/edit")
     end
   end
-  
+
   describe '#update' do
     before do
       @user = Factory(:user)
-      put :update, :id => @user.id, :user => { :resume => "I have updated my resume"
-        }
+      put :update, :id => @user.id, :user => { :resume => "I have updated my resume"}
     end
 
     it "should update the users resume" do
-      @user.reload.first_name.should == "I have updated my resume"
+      @user.reload.resume.should == "I have updated my resume"
     end
   end
 end
