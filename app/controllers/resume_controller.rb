b12759eb66c2d9cd0cf7d2064b76f538d9ae8914
Ryan Resella <ryan@codeforamerica.org>
2011-07-27 12:08:58 -0700

    Added edit path on controller

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -1,5 +1,16 @@
 class ResumeController < ApplicationController
   def index
+
   end
 
+  # GET /resumes/1/edit
+  def edit
+    @user = User.find_by_id(current_user.id)
+  end
+
+  # PUT /resumes/1
+  # PUT /resumes/1.json
+  def update
+
+  end
 end
