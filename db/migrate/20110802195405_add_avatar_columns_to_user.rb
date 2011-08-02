25e230367393d9f57944154441202b545a3984b8
Ryan Resella <ryan@codeforamerica.org>
2011-08-02 12:59:23 -0700

    Added avatar migration for users profile photo

---
+++ db/migrate/20110802195405_add_avatar_columns_to_user.rb
@@ -0,0 +1,8 @@
+class AddAvatarColumnsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :avatar_file_name,    :string
+    add_column :users, :avatar_content_type, :string
+    add_column :users, :avatar_file_size,    :integer
+    add_column :users, :avatar_updated_at,   :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110722021509) do
+ActiveRecord::Schema.define(:version => 20110802195405) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -125,6 +125,10 @@ ActiveRecord::Schema.define(:version => 20110722021509) do
     t.string   "disability"
     t.string   "security_clearance"
     t.string   "unit"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
