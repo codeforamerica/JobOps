930318e1fd3d9a3b799a8d1f112e976b2765fe5c
Ryan Resella <ryan@codeforamerica.org>
2011-08-02 12:49:52 -0700

    Added Paperclip configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,6 +24,8 @@ JobOps::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
-  
+
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
