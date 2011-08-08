67326fc6684ed00ee2cfc92cb141cc1ea2676408
Ryan Resella <ryan@codeforamerica.org>
2011-08-08 13:45:01 -0700

    Clean up white space

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,25 @@
 class UsersController < ApplicationController
-  
+
   before_filter :authenticate_user!
-  
+
   def index
     @user = User.find_by_id(current_user.id)
     render :template => 'users/profile/show'
   end
-  
+
   def show
     @user = User.find(params[:id])
     render :template => 'users/profile/show'
   end
-  
+
   def edit
     @user = User.find(params[:id])
     render :template => 'users/profile/edit'
   end
-  
+
   def update
     @user = User.find(params[:id])
-    
+
     respond_to do |format|
          if @user.update_attributes(params[:user])
            format.html { redirect_to(@user, :notice => 'User was successfully updated.') }
@@ -30,5 +30,5 @@ class UsersController < ApplicationController
          end
     end
   end
-  
+
 end
