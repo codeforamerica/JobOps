96f3450ec1ac4faca66e7b0ae22b397a9d1b6ea2
Ryan Resella <ryan@codeforamerica.org>
2011-08-09 14:55:26 -0700

    Update to use omniauth instead of rack

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -4,10 +4,27 @@ class AuthenticationsController < ApplicationController
   end
 
   def create
-    auth = request.env["rack.auth"]
-    current_user.authentications.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
-    flash[:notice] = "Authentication successful."
-    redirect_to authentications_url
+    omniauth = request.env["omniauth.auth"]
+    authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
+
+    if authentication
+      flash[:notice] = "Signed in successfully."
+      sign_in_and_redirect(:user, authentication.user)
+    elsif current_user
+      current_user.authentications.create!(:provider => omniauth['provider'], :uid => omniauth['uid'])
+      flash[:notice] = "Authentication successful."
+      redirect_to authentications_url
+    else
+      user = User.new
+      user.apply_omniauth(omniauth)
+      if user.save
+        flash[:notice] = "Signed in successfully."
+        sign_in_and_redirect(:user, user)
+      else
+        session[:omniauth] = omniauth.except('extra')
+        redirect_to new_user_registration_url
+      end
+    end
   end
 
   def destroy
