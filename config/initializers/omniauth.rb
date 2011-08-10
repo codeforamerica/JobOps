52b0742fc9effc5be9cc1eaa0239a2b43550164d
Ryan Resella <ryan@codeforamerica.org>
2011-08-09 17:28:16 -0700

    Add LinkIn support

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :linked_in, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
 end
