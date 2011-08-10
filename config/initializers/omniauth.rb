054d7437f72a5f9e1c5685781231599f392eeb35
Ryan Resella <ryan@codeforamerica.org>
2011-08-09 17:50:45 -0700

    Add support for facebook setup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :linked_in, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :setup => true
 end
