4c6ba7f21ffa59bc3845e4913921ea82117f8256
Ryan Resella <ryan@codeforamerica.org>
2011-08-17 15:57:17 -0700

    Add token and secret to attr_accessible

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,5 +1,5 @@
 class Authentication < ActiveRecord::Base
-  attr_accessible :user_id, :provider, :uid
+  attr_accessible :user_id, :provider, :uid, :access_token, :access_secret
 
   belongs_to :user
 end
