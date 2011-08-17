8cfd15d79a4efefe5d168461c39f5208ab201f66
Ryan Resella <ryan@codeforamerica.org>
2011-08-17 15:55:20 -0700

    Added oauth_token and oauth_secret to authentications

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110816223625) do
+ActiveRecord::Schema.define(:version => 20110817225430) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(:version => 20110816223625) do
     t.string   "uid"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "access_token"
+    t.string   "access_secret"
   end
 
   create_table "awards", :force => true do |t|
