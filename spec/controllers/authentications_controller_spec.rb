182846ca4335f84fc81f9fcf1ccc896507befad8
Ryan Resella <ryan@codeforamerica.org>
2011-08-18 16:39:03 -0700

    Wrote spec index, auth_failure, destroy on authentications controller

--- spec/controllers/authentications_controller_spec.rb
+++ spec/controllers/authentications_controller_spec.rb
@@ -1,12 +1,39 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe AuthenticationsController do
-  fixtures :all
-  render_views
+  login_user
 
-  it "index action should render index template" do
-    get :index
-    response.should render_template(:index)
+  describe "GET 'index'" do
+    it "should be successful" do
+      get 'index'
+      response.should be_success
+    end
   end
+
+
+  describe "auth_failure action should render authentication failure template" do
+    it "should be successful" do
+      get :auth_failure
+      response.should render_template("authentications/auth_failure")
+    end
+  end
+
+  describe "destroy action" do
+    before do
+      @auth = Factory(:authentication)
+      @auth_count = Authentication.all.size
+      delete :destroy, :id => @auth.id
+    end
+
+    it "should destroy an authentication" do
+      Authentication.all.size.should == @auth_count -1
+      expect{@auth.reload}.to raise_error ActiveRecord::RecordNotFound
+    end
+
+    it "should redirect to  authentications_url" do
+      @response.should redirect_to(authentications_url)
+    end
+  end
+
 
 end
