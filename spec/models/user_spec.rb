53096a9d7b143f88c1a1dd8cdc8d626226061d76
Ryan Resella <ryan@codeforamerica.org>
2011-08-18 15:03:36 -0700

    Wrote spec for build_authentications

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,9 @@ describe User do
 
   before do
     @user = Factory(:user)
+    @omniauth = {"provider"=>"twitter","uid"=>"12345",
+        "credentials"=>{"token"=>"abc123",
+        "secret"=>"xyz456"}}
   end
 
   describe "age" do
@@ -11,6 +14,19 @@ describe User do
       @age = User.new
       user_age = @age.age(Date.parse("1986-09-07"))
       user_age.should == 24
+    end
+  end
+
+  describe "#build_athentications" do
+    before do
+      @test = User.new.build_authentications(@omniauth)
+    end
+
+    it "should add an authentication" do
+      @test.provider.should == "twitter"
+      @test.uid.should == "12345"
+      @test.access_token.should == "abc123"
+      @test.access_secret.should == "xyz456"
     end
   end
 end
