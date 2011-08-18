7cec84bcbf5182a5608167d5c9f768b6db12c09f
Ryan Resella <ryan@codeforamerica.org>
2011-08-17 17:01:44 -0700

    Revert "Moved twitter initializer into app controller"
    This reverts commit d224209e4ac338132903c4e07558781a66323960.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  private
-
-  def client
-    Twitter.configure do |config|
-      config.consumer_key = ENV['TWITTER_KEY']
-      config.consumer_secret = ENV['TWITTER_SECRET']
-      config.oauth_token = session['access_token']
-      config.oauth_token_secret = session['access_secret']
-    end
-    @client ||= Twitter::Client.new
-  end
 end
