d49e2e487dba17f5083512f635e364267a39ac90
Ryan Resella <ryan@codeforamerica.org>
2011-08-22 16:54:40 -0700

    Wrote spec for new Facebook client

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -40,6 +40,26 @@ describe User do
       @twitter.should be_a Twitter::Client
     end
 
+    it "should not return a new Twitter client" do
+      @twitter = User.new.twitter_user(999999)
+      @twitter.should be_nil
+    end
   end
 
+  describe "#facebook_client" do
+    before do
+      @auth = Factory(:authentication, :provider => "facebook")
+    end
+
+    it "should return a new Facebook client" do
+      @facebook = User.new.facebook_user(@user.id)
+      @facebook.should be_a Mogli::Client
+    end
+
+    it "should not reutrn a new Facebook client" do
+      @facebook = User.new.facebook_user(99999)
+      @facebook.should be_nil
+    end
+  end
 end
+
