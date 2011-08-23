591faff0998b17d74680a6aaefa67912dae1811f
Ryan Resella <ryan@codeforamerica.org>
2011-08-23 16:31:08 -0700

    Added webmock info to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'simplecov'
 SimpleCov.start
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'webmock/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -48,5 +49,12 @@ RSpec.configure do |config|
            'credentials' => {:token => 'abc123', :secret => 'xyz987'}
             }
 
+def fixture_path
+  File.expand_path('../fixtures', __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
+end
 
 end
