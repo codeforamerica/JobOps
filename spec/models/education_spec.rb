74b9b0406c1e0ad4e526e4a8057f26c0dde9585a
Ryan Resella <ryan@codeforamerica.org>
2011-08-24 18:22:27 -0700

    Cleanup whitespace

--- spec/models/education_spec.rb
+++ spec/models/education_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe Education do
-  
+
   describe "check if valid year" do
     year = Education.new.valid_year(Date.today)
     year.should == Date.today.year
   end
-  
+
   describe "blank year should return nil" do
     year = Education.new.valid_year(nil)
     year.should == nil
