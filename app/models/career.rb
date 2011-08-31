7d1e117777d736af5ea5ab99b61f835b3b336f79
Ryan Resella <ryan@codeforamerica.org>
2011-08-31 16:08:11 -0700

    Implemented futures pipeline client

--- app/models/career.rb
+++ app/models/career.rb
@@ -1,2 +1,13 @@
 class Career < ActiveRecord::Base
+
+  def futures_pipeline
+    futures_client
+  end
+
+  protected
+
+  def futures_client
+    futures_client ||= FuturesPipeline::Client.new
+  end
+
 end
