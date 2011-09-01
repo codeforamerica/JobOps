29e5f1d8aba2e9345bd73faf564c7aaf454c098f
Ryan Resella <ryan@codeforamerica.org>
2011-09-01 12:52:27 -0700

    Added spec for show on careers controller

--- spec/controllers/careers_controller_spec.rb
+++ spec/controllers/careers_controller_spec.rb
@@ -11,4 +11,14 @@ describe CareersController do
     end
   end
 
+  describe "#show" do
+    it "should render the show template" do
+      stub_request(:get, "http://militarydemo.pipelinenc.com/api/v1/careers/11-1021-00.json").
+        to_return(:status => 200, :body => fixture("futures_career.json"))
+      get 'show', :id => '11-1021-00'
+      response.should render_template("careers/show")
+    end
+  end
+
+
 end
