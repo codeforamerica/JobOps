d3c2be1bca2b2d6c5f2e876fa676f30e6665f893
Ryan Resella <ryan@codeforamerica.org>
2011-09-01 15:53:40 -0700

    Updated spec mock for paging

--- spec/controllers/careers_controller_spec.rb
+++ spec/controllers/careers_controller_spec.rb
@@ -4,7 +4,7 @@ describe CareersController do
 
   describe "GET 'index'" do
     it "should be successful" do
-      stub_request(:get, "http://militarydemo.pipelinenc.com/api/v1/careers.json").
+      stub_request(:get, "http://militarydemo.pipelinenc.com/api/v1/careers.json?page=").
         to_return(:status => 200, :body => fixture("futures_careers.json"))
       get 'index'
       response.should be_success
@@ -19,6 +19,5 @@ describe CareersController do
       response.should render_template("careers/show")
     end
   end
-
 
 end
