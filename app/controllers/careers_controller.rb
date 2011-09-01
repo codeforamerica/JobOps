3ad63afe0e6e0e3b8cb4e66b1c7313c1d6d491e7
Ryan Resella <ryan@codeforamerica.org>
2011-09-01 12:53:40 -0700

    Move futures_pipeline into before filter and created show method

--- app/controllers/careers_controller.rb
+++ app/controllers/careers_controller.rb
@@ -1,9 +1,22 @@
 class CareersController < ApplicationController
+  before_filter :get_career
+
+  def get_career
+    @futures_careers = Career.new.futures_pipeline
+  end
+
   def index
-    @careers = Career.new.futures_pipeline.careers
+    @careers = @futures_careers.careers
   end
 
   def show
+    @careers = @futures_careers.career(params[:id])
+
+   respond_to do |format|
+      format.html # show.html.erb
+      format.json { render :json => @careers }
+    end
+
   end
 
 end
