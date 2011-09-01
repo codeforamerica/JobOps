1f311d8158e9b6da2ac3445910313b5a16653b69
Ryan Resella <ryan@codeforamerica.org>
2011-08-31 17:36:21 -0700

    Added stub request to spec for career controller

--- spec/controllers/careers_controller_spec.rb
+++ spec/controllers/careers_controller_spec.rb
@@ -4,6 +4,8 @@ describe CareersController do
 
   describe "GET 'index'" do
     it "should be successful" do
+      stub_request(:get, "http://militarydemo.pipelinenc.com/api/v1/careers.json").
+        to_return(:status => 200, :body => fixture("futures_careers.json"))
       get 'index'
       response.should be_success
     end
