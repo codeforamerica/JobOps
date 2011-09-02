ad303a43177847667973a50f7f5bda3a20f2f3f4
Ryan Resella <ryan@codeforamerica.org>
2011-09-01 17:10:52 -0700

    Added spec for paging through futures_pipeline careers

--- spec/controllers/careers_controller_spec.rb
+++ spec/controllers/careers_controller_spec.rb
@@ -3,10 +3,17 @@ require 'spec_helper'
 describe CareersController do
 
   describe "GET 'index'" do
-    it "should be successful" do
+    it "should list 50 careers at a time" do
       stub_request(:get, "http://militarydemo.pipelinenc.com/api/v1/careers.json?page=").
         to_return(:status => 200, :body => fixture("futures_careers.json"))
       get 'index'
+      response.should be_success
+    end
+
+    it "should return paged results from a career" do
+      stub_request(:get, "http://militarydemo.pipelinenc.com/api/v1/careers.json?page=7").
+        to_return(:status => 200, :body => fixture("futures_careers.json"))
+      get 'index', :page => 7
       response.should be_success
     end
   end
