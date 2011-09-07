9de87bc844cef1eda1674c70aa27c5d023ba6eec
Ryan Resella <ryan@codeforamerica.org>
2011-09-06 18:28:11 -0700

    Generated Location model

---
+++ db/migrate/20110907012712_create_locations.rb
@@ -0,0 +1,11 @@
+class CreateLocations < ActiveRecord::Migration
+  def change
+    create_table :locations do |t|
+      t.string :location
+      t.float :lat
+      t.float :long
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110907005112) do
+ActiveRecord::Schema.define(:version => 20110907012712) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -119,6 +119,14 @@ ActiveRecord::Schema.define(:version => 20110907005112) do
   create_table "languages", :force => true do |t|
     t.string   "language"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "locations", :force => true do |t|
+    t.string   "location"
+    t.float    "lat"
+    t.float    "long"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
