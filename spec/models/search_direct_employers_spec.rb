fcb31caf542aa5ff71e221c6c881b63c9817002c
Ryan Resella <ryan@codeforamerica.org>
2011-09-07 15:41:41 -0700

    Added initial specs for direct employers

--- spec/models/search_direct_employers_spec.rb
+++ spec/models/search_direct_employers_spec.rb
@@ -1,8 +1,24 @@
 require 'spec_helper'
 
 describe SearchDirectEmployers do
-  context "it calls the api with params" do
-    pending "it should return a set of results by page params and keyword"
+  describe "#direct_client" do
+    it "should return a new DirectEmployers client" do
+      @direct = SearchDirectEmployers.new.direct_client
+      @direct.should be_a DirectEmployers::Client
+    end
   end
-  
+
+  describe "#direct_search_moc" do
+    before do
+      @direct = SearchDirectEmployers.new.direct_client
+      stub_request(:get, "http://www.jobcentral.com/api.asp?key=&moc=11b").
+        to_return(:status => 200, :body => fixture("direct_employers_11b.xml"))
+    end
+
+    it "should return a list of jobs related to MOC" do
+      @results = @direct.search({:moc => "11b"})
+      @results.api.jobs.job.last.company = "Code for America"
+    end
+  end
+
 end
