7156255e98707e46132980ee04bcde6561d3935f
Dan Melton <melton.dan@gmail.com>
2011-09-07 14:12:00 -0700

    Added company specs

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,5 @@
 class Company < ActiveRecord::Base
   has_many :jobs
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Company do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @company = Factory(:company)
+  end
+  context "relationships" do
+    it 'has many jobs' do
+      @company.respond_to?(:jobs).should be_true      
+    end
+  end
+  
+  context "validations" do
+    it "has to have a name" do
+      @company.name = nil
+      @company.should have(1).error_on(:name)
+    end
+    
+    it "has to have a unique company" do
+      attr = Factory.attributes_for(:company)
+      invalid_detail = Company.new(attr)
+      invalid_detail.should have(1).error_on(:company)      
+    end
+    
+  end
+  
+  
 end
