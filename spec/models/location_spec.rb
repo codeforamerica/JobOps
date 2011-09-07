76233b2845da9380ce1fdc667ff776de20182f1c
Dan Melton <melton.dan@gmail.com>
2011-09-07 14:07:19 -0700

    Added location spec

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -2,11 +2,31 @@ require 'spec_helper'
 
 describe Location do
   before do
-    @location = Factory(:location)
+    stub_request(:get, "http://maps.google.com/maps/api/geocode/json?address=San%20Francisco,%20CA&language=en&sensor=false").
+     with(:headers => {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
+     to_return(:status => 200, :body => fixture("google_map_location_sfca.json"), :headers => {})
+    
+    @location = Factory(:location, :location => "San Francisco, CA")
   end
   context "relationships" do
     it 'has many jobs' do
       @location.respond_to?(:jobs).should be_true      
     end
   end
+  
+  context "validations" do
+    it "has to have a location" do
+      @location.location = nil
+      @location.should have(1).error_on(:location)
+    end
+    
+    it "has to have a unique location" do
+      attr = Factory.attributes_for(:location, :location => "San Francisco, CA")
+      invalid_detail = Location.new(attr)
+      invalid_detail.should have(1).error_on(:location)      
+    end
+    
+  end
+  
+  
 end
