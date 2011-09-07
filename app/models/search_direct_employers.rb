f2c4bd817cc8d0b1c3fe20310b335e7061b087cc
Ryan Resella <ryan@codeforamerica.org>
2011-09-07 15:42:00 -0700

    Added direct employers initializer

--- app/models/search_direct_employers.rb
+++ app/models/search_direct_employers.rb
@@ -1,5 +1,15 @@
-class SearchDirectEmployers < ActiveRecord::Base
+class SearchDirectEmployers
 
+  def direct_client
+    direct_employers_client
+  end
+
+
+  protected
+
+  def direct_employers_client
+    @client ||= DirectEmployers.new({:key => ENV['DIRECT_EMPLOYERS']})
+  end
 
 end
 
