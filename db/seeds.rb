ed53c22fedbfeb79fdaaaf8a051e519451840cc5
Ryan Resella <ryan@codeforamerica.org>
2011-09-08 12:17:19 -0700

    Updated seed file for data model

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,17 @@
 if Rails.env != 'production'
 
   100.times do |i|
-    Job.create(:company => Faker::Company.name,
+    Location.create(:location => Faker::Address.city)
+  end
+
+  100.times do |i|
+    Company.create(:name => Faker::Company.name, :location => Faker::Address.city)
+  end
+
+  100.times do |i|
+    Job.create(:company_id => i ,
       :date_acquired => Time.at(Time.local(2011,1,1) + rand * (Time.now - Time.local(2011,12,31))),
-      :location => Faker::Address.city,
+      :location_id => i ,
       :title => Faker::Company.catch_phrase,
       :url => Faker::Internet.domain_name)
   end
