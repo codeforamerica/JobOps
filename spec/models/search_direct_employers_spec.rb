bccf3ac91420827f9b81c2d8abe21117142353d7
Ryan Resella <ryan@codeforamerica.org>
2011-09-07 19:23:04 -0700

    Remove unused test

--- spec/models/search_direct_employers_spec.rb
+++ spec/models/search_direct_employers_spec.rb
@@ -7,18 +7,4 @@ describe SearchDirectEmployers do
       @direct.should be_a DirectEmployers::Client
     end
   end
-
-  describe "#direct_search_moc" do
-    before do
-      @direct = SearchDirectEmployers.new.direct_client
-      stub_request(:get, "http://www.jobcentral.com/api.asp?key=&moc=11b").
-        to_return(:status => 200, :body => fixture("direct_employers_11b.xml"))
-    end
-
-    it "should return a list of jobs related to MOC" do
-      @results = @direct.search({:moc => "11b"})
-      @results.api.jobs.job.last.company = "Code for America"
-    end
-  end
-
 end
