730428dce8171fdea216e465bce1e7a5812b1d00
Ryan Resella <ryan@codeforamerica.org>
2011-09-08 23:51:39 -0700

    Updating tests to pass on 1.8.7

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -3,11 +3,9 @@ require 'spec_helper'
 describe JobsController do
   before do
     stub_request(:get, "http://maps.google.com/maps/api/geocode/json?address=San%20Francisco,%20CA&language=en&sensor=false").
-             with(:headers => {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
-             to_return(:status => 200, :body => fixture("google_map_location_sfca.json"), :headers => {})
-     stub_request(:get, "http://militarydemo.pipelinenc.com/api/v1/careers/search.json?moc=11B").
+      to_return(:status => 200, :body => fixture("google_map_location_sfca.json"), :headers => {})
+    stub_request(:get, "http://militarydemo.pipelinenc.com/api/v1/careers/search.json?moc=11B").
        to_return(:status => 200, :body => fixture("futures_11b.json"))
-             
   end
 
   describe "#index" do
@@ -29,7 +27,7 @@ describe JobsController do
     end
 
   end
-  
+
   describe "#flag" do
     before do
       @user = Factory(:user)
@@ -42,14 +40,14 @@ describe JobsController do
       get :flag, :id => @job
       }.should change(JobUser, :count).by(1)
     end
-    
+
     it "should deflag a job" do
       @user.jobs << @job
       lambda {
       get :flag, :id => @job
       }.should change(JobUser, :count).by(-1)
-    end    
-    
+    end
+
   end
 
 
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -3,59 +3,58 @@ require 'spec_helper'
 describe Job do
   before do
     stub_request(:get, "http://maps.google.com/maps/api/geocode/json?address=San%20Francisco,%20CA&language=en&sensor=false").
-     with(:headers => {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
-     to_return(:status => 200, :body => fixture("google_map_location_sfca.json"), :headers => {})
+      to_return(:status => 200, :body => fixture("google_map_location_sfca.json"), :headers => {})
     @company = Factory(:company)
     @location = Factory(:location, :location => "San Francisco, CA")
     @job=Factory(:job, :location => @location)
   end
-  
+
   context "validations" do
     it 'presence of title' do
       @job.title = nil
-      @job.should have(1).error_on(:title)      
+      @job.should have(1).error_on(:title)
     end
 
     it 'presence of location' do
       @job.location = nil
-      @job.should have(1).error_on(:location)      
-      
+      @job.should have(1).error_on(:location)
+
     end
 
     it 'presence of company' do
       @job.company = nil
-      @job.should have(1).error_on(:company)      
-      
+      @job.should have(1).error_on(:company)
+
     end
-    
+
     it 'presence of url' do
       @job.url = nil
-      @job.should have(1).error_on(:url)      
+      @job.should have(1).error_on(:url)
     end
-    
+
   end
-  
+
   context "relationships" do
     it 'has many job searches' do
       @job.respond_to?(:job_searches).should be_true
     end
     it 'belongs to location' do
-      @job.respond_to?(:location).should be_true      
+      @job.respond_to?(:location).should be_true
     end
     it 'belongs to company' do
-      @job.respond_to?(:company).should be_true      
+      @job.respond_to?(:company).should be_true
     end
     it 'has many job searches jobs' do
       @job.respond_to?(:job_searches_jobs).should be_true
     end
     it 'has many jobs' do
       @job.respond_to?(:job_searches).should be_true
-    end    
+    end
     it 'has many user flags' do
       @job.respond_to?(:users).should be_true
     end
   end
-  
+
   context "job specific checks" do
     pending "it should grab the url if its a redirect"
   end
