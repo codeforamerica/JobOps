f9c83013e7f77726427134b5c6ee82af8129cb8a
Ryan Resella <ryan@codeforamerica.org>
2011-09-09 12:18:37 -0700

    Updated config to only pull in env keys on development

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Load heroku vars from local file
 heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
-load(heroku_env) if File.exists?(heroku_env)
+if Rails.env.developemnt?
+  load(heroku_env) if File.exists?(heroku_env)
+end
 
 # Initialize the rails application
 JobOps::Application.initialize!
