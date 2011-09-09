5fe42fe1a495feed7f0e58e8ce2b2894a52c291e
Dan Melton <melton.dan@gmail.com>
2011-09-09 14:46:13 -0700

    Fixed company nil problem

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ActiveRecord::Base
   has_many :jobs
   validates_presence_of :name
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, :scope => :location
   
 end
--- app/models/job_search.rb
+++ app/models/job_search.rb
@@ -1,7 +1,7 @@
 class JobSearch < ActiveRecord::Base
   has_many :job_searches_users
   has_many :users, :through => :job_searches_users
-  has_many :job_searches_jobs
+  has_many :job_searches_jobs, :dependent => :destroy
   has_many :jobs, :through => :job_searches_jobs
 
   def search
