d3d621fd7d9a1492a797184672f4bd4dcc56efef
Ryan Resella <ryan@codeforamerica.org>
2011-09-12 18:24:03 -0700

    Wrote specs for jobs searches user controller

--- spec/controllers/job_searches_user_controller_spec.rb
+++ spec/controllers/job_searches_user_controller_spec.rb
@@ -2,10 +2,27 @@ require 'spec_helper'
 
 describe JobSearchesUserController do
 
+  stub_user_moc_save
+  login_user
+
   describe "GET 'index'" do
     it "should be successful" do
       get 'index'
       response.should be_success
+    end
+  end
+
+  describe "#create" do
+    before do
+      post :create, :search => Factory.attributes_for(:job_search)
+    end
+    pending it "should create a saved search" do
+
+    end
+  end
+
+  describe "#destroy" do
+    pending it "should delete a saved search" do
     end
   end
 
