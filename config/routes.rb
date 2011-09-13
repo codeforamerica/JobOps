5f30639377f5b39340b376d497c87147b29ab83a
Ryan Resella <ryan@codeforamerica.org>
2011-09-12 20:20:34 -0700

    Udpate routes for new saves search

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 JobOps::Application.routes.draw do
 
-  get "job_searches_user/index"
+  get "job_searches_user/new/", :controller => :job_searches_user, :action => :new
 
   resources :jobs, :only => [:index, :show]
   get "/jobs/flag/:id", :controller => :jobs, :action => :flag, :defaults => { :format => 'json' }, :as => :flag_job
