0203f6722a10376f44c343ddf03e986729115cef
Erik Michaels-Ober <sferik@gmail.com>
2011-09-13 17:01:17 -0700

    Add specs for the case where a Google Place is not found

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -4,7 +4,7 @@ describe Company do
   before do
     stub_request(:get, "http://maps.google.com/maps/api/geocode/json?address=San%20Francisco,%20CA&language=en&sensor=false").
       to_return(:status => 200, :body => fixture("google_map_location_sfca.json"), :headers => {})
-    @company = Factory(:company, :location => "San Francisco, CA", :name => "Code for America")
+    @company = Factory(:company, :location => "San Francisco, CA", :name => "Code for America", :lat => 37, :long => -122)
   end
   context "relationships" do
     it 'has many jobs' do
@@ -40,6 +40,16 @@ describe Company do
       @company.lat.should == 37.788299
       @company.long.should == -122.399983
     end
-  end
 
+    it "should not geocode the place if not found in Google Places" do
+      stub_request(:get, "https://maps.googleapis.com/maps/api/place/search/json?key=love&location=37.0,-122.0&name=Code%20for%20America&radius=500&sensor=false").
+        to_return(:status => 200, :body => fixture("place_not_found.json"), :headers => {:content_type => "application/json; charset=UTF-8"})
+      @company.update_location_if_found_in_google_places
+      a_request(:get, "https://maps.googleapis.com/maps/api/place/search/json?key=love&location=37.0,-122.0&name=Code%20for%20America&radius=500&sensor=false").
+        should have_been_made
+      @company.lat.should == 37
+      @company.long.should == -122
+    end
+
+  end
 end
