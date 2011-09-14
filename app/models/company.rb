53dd67bcedb28d79fe2f7bfb0905aafd35c79ee1
Erik Michaels-Ober <sferik@gmail.com>
2011-09-13 17:02:41 -0700

    Fix Company#update_location_if_found_in_google_places to not raise an exception when a Google Place is not found

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,25 +1,24 @@
+require 'places'
+
 class Company < ActiveRecord::Base
   has_many :jobs
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => :location
   geocoded_by :address
-  after_validation :geocode   
-  
+  after_validation :geocode
+
   def address
     self.location
   end
-  
+
   def update_location_if_found_in_google_places
     @client = Places::Client.new(:api_key => ENV['PLACES'])
-    search = @client.search(:lat => self.lat, :lng => self.long, :name => self.name)
-    puts search.inspect
-    geometry = search.results.first.geometry
-    if geometry.first.last
-      self.lat = geometry.first.last.lat
-      self.long = geometry.first.last.lng
+    search = @client.search(:lat => lat, :lng => long, :name => name)
+    if !search.results.empty?
+      geometry = search.results.first.geometry.first.last
+      self.lat = geometry.lat
+      self.long = geometry.lng
       self.save
     end
   end
-  
-  
 end
