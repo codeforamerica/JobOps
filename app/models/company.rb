7d7f6475d3bfebf45f2cc9f548c0eaab848d8bc9
Dan Melton <melton.dan@gmail.com>
2011-09-14 13:53:32 -0700

    Fixed company and location lat and long issues [Delivers #18304767]

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,17 +2,12 @@ class Company < ActiveRecord::Base
   has_many :jobs
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => :location
-  geocoded_by :address
-  after_validation :geocode   
-  
-  def address
-    self.location
-  end
+  geocoded_by :location, :latitude => :lat, :longitude => :long
+  after_create :geocode
   
   def update_location_if_found_in_google_places
     @client = Places::Client.new(:api_key => ENV['PLACES'])
     search = @client.search(:lat => self.lat, :lng => self.long, :name => self.name)
-    puts search.inspect
     geometry = search.results.first.geometry
     if geometry.first.last
       self.lat = geometry.first.last.lat
