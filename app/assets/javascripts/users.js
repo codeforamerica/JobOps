abeb37e0e31dfb8073f5cbcc26cb7de025d8f1c2
John Mertens <john.mertens@gmail.com>
2011-09-15 11:30:24 -0700

    Added tab functionality to the profile page.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,20 @@
 $(document).ready(function() {
+  // Initialize our tabs
+  $('.tab_content').hide();
+  $('.tab_content').first().show();
+
+  // Bind the tab buttons to the tab panels.
+  // We just read the href of the <A> and look for an element with that ID.
+  // If we find the element, we display it, if not, we do nothing.
+  $('ul.tabs a').click(function(ev) {
+    ev.preventDefault();
+    var targetClass = this.href.split('#').pop();
+    var $target = $('#'+targetClass);
+    if($target.length) {
+      if($('.tab_content:visible').attr('id') !== targetClass) {
+        $('.tab_content:visible').fadeOut(function() { $target.fadeIn() });
+      }
+    }
+  });
+
 });
