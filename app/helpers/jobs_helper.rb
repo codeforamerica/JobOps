8d234ca2b60f404901e6bba0a6de425ad5ec7760
Dan Melton <melton.dan@gmail.com>
2011-09-15 15:49:36 -0700

    Adding tips

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,27 @@
 module JobsHelper
+  
+  def save_search_tip
+    "Saving job searches allows you to easily check important search terms each time you start a session on JobOps. Start saving searches and they will appear here. You can also update your MOC code in <a href='/users'>your profile</a>, and we will generate some job searches that fit your background."
+  end
+  
+  def smart_filter_tip
+    "Turning on Smart Filters helps you easily apply a set of filters to every job search you make, helping you to find the jobs you want, and eliminating results that don't apply to you. Smart Filters work especially well if your searches are tied to a specific location, job title, or company. If the Smart Filters slider is set to On, your searches will only return results that fit the Smart Filters you have set. You can change these filters at any time. To turn off Smart Filters, change the Smart Filters setting to Off."
+  end
+  
+  def save_search_blank
+    "You don't have any saved searches. Saving job searches makes it easier to check important search terms each time you come to JobOps, so you don't have to search from scratch each time you come to JobOps. You can start saving searches that are relevant to you, or you can update your MOC code in <a href='/users'>your profile</a> to generate some recommended saved searches."
+  end
+ 
+  def flagged_jobs_blank
+    "You don't have any flagged jobs. Flag jobs that interest you by clicking on the star next to job result title. Your flagged jobs will appear here; when you've flagged jobs, you can compare them [here]."
+  end
+
+  def flagged_jobs_tip
+    "Flagging jobs saves individual job listings so that you can more easily access and compare them later. By flagging noteworthy job listings, you can better organize, compare, share, and apply for jobs; and your flagged jobs list is saved so you can access it whenever you log-in to JobOps."
+  end
+  
+  def flag_tip
+    "Clicking on the star next to the job listing flags it so you can easily find it later. Compare all of your flagged jobs [here]."
+  end
+
 end
