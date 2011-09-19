da8e66b912e41bf6d84753c9ef1458143e391558
John Mertens <john.mertens@gmail.com>
2011-09-19 13:37:43 -0700

    Added tooltip function definitions for the military profile tab.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,6 +10,20 @@ module UsersHelper
   end
   def profile_desired_salary_tip
   end
+  def profile_military_status_tip
+  end
+  def profile_service_branch_tip
+  end
+  def profile_moc_tip
+  end
+  def profile_rank_tip
+  end
+  def profile_unit_tip
+  end
+  def profile_disability_tip
+  end
+  def profile_security_clearance_tip
+  end
   def linkedin_tip
   "some content here"
   end
