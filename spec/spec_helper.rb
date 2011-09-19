3a2dee44f029f31f171a722e65ef853c0d164b55
Ryan Resella <ryan@codeforamerica.org>
2011-09-19 13:18:11 -0700

    Set SimpleCov to use rails config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ Spork.prefork do
   # need to restart spork for it take effect.
   ENV["RAILS_ENV"] ||= 'test'
   require 'simplecov'
-  SimpleCov.start
+  SimpleCov.start 'rails'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'webmock/rspec'
