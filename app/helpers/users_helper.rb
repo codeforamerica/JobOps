b8362c7dd20f11ad7ea810c511e7ad822e2d35ad
Matt Lewis <mattrlewis2@gmail.com>
2011-09-19 13:52:59 -0700

    added tips to military info edit

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -15,18 +15,25 @@ module UsersHelper
     "In dollars, the median amount you are seeking."
   end
   def profile_military_status_tip
+    "e.g. active, retired, etc."
   end
   def profile_service_branch_tip
+    "e.g. Army, Navy, Marines, Air Force, Coast Guard."
   end
   def profile_moc_tip
+    "Entering in your military occupational code helps us recommend jobs and job searches for you."
   end
   def profile_rank_tip
+    "Your most recent rank in the service."
   end
   def profile_unit_tip
+    "The most recent unit with whcih you served."
   end
   def profile_disability_tip
+    "If applicable, your level of disability (e.g. 30%). If not eligible, leave blank."
   end
   def profile_security_clearance_tip
+    "Some jobs look for applications with security clearance; entering yours helps us show you those."
   end
   def linkedin_tip
   "some content here"
