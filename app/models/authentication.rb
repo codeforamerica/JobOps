1306df8d57b85884069d28804c97f7f1af767c4b
Ryan Resella <ryan@codeforamerica.org>
2011-09-20 15:25:11 -0700

    Added scope for facebook,linkedin, twitter

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -2,5 +2,9 @@ class Authentication < ActiveRecord::Base
   attr_accessible :user_id, :provider, :uid, :access_token, :access_secret
 
   belongs_to :user
+
+  scope :facebook, where(:provider => 'facebook')
+  scope :linked_in, where(:provider => 'linked_in')
+  scope :twitter, where(:provider => 'twitter')
 end
 
