6e597a04d400ad190cf0a85fd672b5c5c58cda40
Ryan Resella <ryan@codeforamerica.org>
2011-09-26 14:07:41 -0700

    Added spec for relationships on careers

--- spec/models/career_spec.rb
+++ spec/models/career_spec.rb
@@ -5,8 +5,15 @@ describe Career do
     it "should return a new FuturesPipeline client" do
       @test = Career.new.futures_pipeline
       @test.should be_a FuturesPipeline::Client
+    end
   end
 
+  describe "relationships" do
+    before do
+      @career = Factory(:career)
+    end
+    it 'has many user flags' do
+      @career.respond_to?(:users).should be_true
+    end
   end
-
 end
