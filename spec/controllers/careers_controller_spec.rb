3f7afd7e601eb717fc47692c4480a074f79b9c11
Ryan Resella <ryan@codeforamerica.org>
2011-09-28 11:49:14 -0700

    Wrote specs for a blank moc and searching for a blank moc

--- spec/controllers/careers_controller_spec.rb
+++ spec/controllers/careers_controller_spec.rb
@@ -29,14 +29,31 @@ describe CareersController do
     before do
       stub_request(:get, "http://militarydemo.pipelinenc.com/api/v1/careers/search.json?moc=11B").
         to_return(:status => 200, :body => fixture("futures_11b.json"))
-      @user = Factory(:user)
+      stub_request(:get, "http://militarydemo.pipelinenc.com/api/v1/careers.json").
+        to_return(:status => 200, :body => fixture("futures_careers.json"))
     end
 
     it "should render the careers template" do
+      @user = Factory(:user)
       sign_in(@user)
       get :index
       response.should render_template("careers/index")
     end
+
+    it "should render the careers template with no MOC" do
+      @user = Factory(:user, :moc => "")
+      sign_in(@user)
+      get :index
+      response.should render_template("careers/index")
+    end
+
+    it "should render the careers template for a logged in user searching by MOC" do
+      @user = Factory(:user, :moc => "")
+      sign_in(@user)
+      get :index, :moc => "11B"
+      response.should render_template("careers/index")
+    end
+
   end
 
   describe "#show" do
